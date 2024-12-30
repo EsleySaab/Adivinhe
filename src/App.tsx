d7a915b560ff0f9cd12b309e4917775c113e6963
EsleySaab <esleysantana.sa@gmail.com>
2024-12-30 15:57:24 -0300

    feat: creating the tips component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import styles from "./App.module.css"
 
 import { Header } from "./components/Header/index"
 
+import { Tip } from "./components/Tip"
+
 function App() {
   function handleRestartGame() {
     alert("Reiniciar o jogo")
@@ -10,7 +12,9 @@ function App() {
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={2} max={10} onRestart={handleRestartGame} />
+
+        <Tip tip="Uma das linguagens de programação mais utilizadas" />
       </main>
     </div>
   )
