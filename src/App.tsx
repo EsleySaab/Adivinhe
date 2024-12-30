418abec6902767765f7b6fc117dd8be256344572
EsleySaab <esleysantana.sa@gmail.com>
2024-12-30 16:30:56 -0300

    feat: creating the letter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import styles from "./App.module.css"
 
 import { Header } from "./components/Header/index"
-
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 function App() {
   function handleRestartGame() {
@@ -15,6 +15,12 @@ function App() {
         <Header current={2} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
       </main>
     </div>
   )
