edf7766bd5d102c2383406664bad99678959ef3a
EsleySaab <esleysantana.sa@gmail.com>
2024-12-31 13:29:46 -0300

    style: styling main

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header/index"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
 
-function App() {
+export function App() {
   function handleRestartGame() {
     alert("Reiniciar o jogo")
   }
@@ -15,15 +15,14 @@ function App() {
         <Header current={2} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
-
-        <Letter value="R" />
-        <Letter value="E" />
-        <Letter value="A" />
-        <Letter value="C" />
-        <Letter value="T" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   )
 }
-
-export default App
